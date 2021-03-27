b6f261cd47a9689dac0139acd9cc590aa92dc000
Brian Forth <brianwforth@gmail.com>
2021-03-27 11:12:25 -0500

    Adding majorAirports.json

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,27 +1,40 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create map object
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create map object with center at SFO
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Coordinates for each point to be used in the line.
-let line = [
-    [37.6213, -122.3790],
-    [30.1975, -97.6664],
-    [43.1398791, -89.3375045],
-    [43.6777, -79.6248],
-    [40.641766, -73.780968]
-];
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "blue",
-    opacity: 0.5,
-    dashArray: "4"
+//Grabbing GeoJSON data
+L.geoJSON(sanFranAirport, {
+    // Turn features into map markers
+    onEachFeature: function(feature, layer){
+        console.log(layer);
+        layer.bindPopup("<h1> Airport Code: " + feature.properties.faa + "</h1><h2> Airport name: " + feature.properties.name);
+    }
 }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
