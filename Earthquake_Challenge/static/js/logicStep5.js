9699fad33ff45efc5a710255cea07210353fd1f2
Brian Forth <brianwforth@gmail.com>
2021-03-27 18:42:34 -0500

    Completed Deliverable 1

---
+++ Earthquake_Challenge/static/js/logicStep5.js
@@ -0,0 +1,121 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// We create the tile layer that will be the background of our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Create map object with center, zoom, and default layer
+let map = L.map('mapid',{
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+});
+// Create base layer to hold both maps
+let baseMaps = {
+    "Streets": streets,
+    "Satellite Streets": satelliteStreets
+};
+// Create earthquake layer for map
+let earthquakes = new L.layerGroup();
+
+// Definte object containing overlays
+let overlays = {
+    "Earthquakes": earthquakes
+};
+// Pass map layers into layers control and add control to map
+L.control.layers(baseMaps, overlays).addTo(map);
+
+// Grabbing GeoJSON data
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data){
+    console.log(data);
+    // Function to return style data for earthquakes
+    function styleInfo(feature){
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: getColor(feature.properties.mag),
+            color: "#000000",
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        }
+    }
+
+    // Function to return radius based on quake magnitude
+    function getRadius(magnitude){
+        if (magnitude === 0){
+            return 1;
+        }
+        return magnitude * 4;
+    }
+
+    // This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+    if (magnitude > 5) {
+        return "#ea2c2c";
+    }
+    if (magnitude > 4) {
+        return "#ea822c";
+    }
+    if (magnitude > 3) {
+        return "#ee9c00";
+    }
+    if (magnitude > 2) {
+        return "#eecc00";
+    }
+    if (magnitude > 1) {
+        return "#d4ee00";
+    }
+    return "#98ee00";
+}
+    // Creating GeoJSON layer with retrieved data
+    L.geoJson(data,{
+        // Features => circleMarkers
+        pointToLayer: function(feature, latlng){
+            console.log(data);
+            return L.circleMarker(latlng);
+        },
+        // Setting style for circleMarkers using styleInfo function (Line 34)
+        style: styleInfo,
+        // Create popups to display magnitude and location of quakes
+        onEachFeature: function(feature, layer){
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+        }
+    }).addTo(earthquakes);
+});
+// Leaflet Legend Code
+let legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function () {
+    let div = L.DomUtil.create('div', 'info legend');
+    const magnitudes = [0,1,2,3,4,5];
+    const colors = [
+        "#98ee00",
+        "#d4ee00",
+        "#eecc00",
+        "#ee9c00",
+        "#ea822c",
+        "#ea2c2c"
+    ];
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < magnitudes.length; i++) {
+        console.log(colors[i]);
+        div.innerHTML +=
+            "<i style='background: " + colors[i] + "'></i> " +
+            magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+    }
+    return div;
+    };
+legend.addTo(map);
+
+// Add earthquakes to map
+earthquakes.addTo(map);
